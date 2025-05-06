afb651353bd83ded4b960af96977723d0037bc7d
Mizan <ghodafail@gmail.com>
2025-05-06 09:41:53 +0700

    Update Soal_1.py

--- Soal_1.py
+++ Soal_1.py
@@ -16,8 +16,7 @@ class View(object):
       print("For", Model.services[svc]['number'], 
             svc, "message you pay $", 
             Model.services[svc]['price']) 
-
-#Pengembangan Kode Soal 1 menjadi Kode Soal 3        
+       
 class Controller(object): 
   def __init__(self): 
     self.model = Model() 
@@ -29,19 +28,11 @@ class Controller(object):
  
   def get_pricing(self): 
     services = self.model.services.keys() 
-    return(self.view.list_pricing(services))
-
-  def bid_price(self):
-    tawar = input("What service do you want to bid? email, sms, or voice : ") 
-    harga = int(input("Enter the price you want (in $) : "))
-    Model.services[tawar]['price'] = harga
-    print("Price according to your bid: ")
-    self.get_pricing()
-
+    return(self.view.list_pricing(services)) 
+ 
 #Instansiasi objek 
 controller = Controller() 
 print("Services Provided:") 
 controller.get_services() 
 print("Pricing for Services:") 
-controller.get_pricing()
-controller.bid_price()
+controller.get_pricing() 
